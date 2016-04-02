eaa95d0f397f3f2ec4c8a4dca5c7816078d177af
justitow <justitow@gmail.com>
2016-04-02 00:16:17 +0000

    Got linked list working

--- timetest.cpp
+++ timetest.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <cstring>
 
 #include "CursorList.h"
 #include "LinkedList.h"
@@ -13,6 +15,16 @@
 vector<CursorNode <int> > cursorSpace(250000);
 
 using namespace std;
+
+void parseCommand(string str, char& command, int& number)
+{
+  char * cstr = new char [str.length()+1];
+  strcpy(cstr, str.c_str());
+  command = cstr[0];
+  cstr++;
+  number = atoi(cstr);
+  //cout << command << number << endl;
+}
 
 int getChoice()
 {
@@ -35,46 +47,135 @@ int getChoice()
 
 void RunList(char* filename)
 {
+  List<int> myList;
+  
+  ifstream inf;
+  inf.open(filename);
+  string str;
   char command;
   int number;
   
-  List<int> myList;
-  
-  ifstream inf(filename);
-  
-  for (int i = 0; i < 500000; i++)
+  while(inf >> str)
   {
-    inf >> command >> number;
+    parseCommand(str, command, number);
+    //cout << command << " " << number << endl;
     if (command == 'i')
+    {
       myList.insert(number, myList.zeroth());
+    }
     else
+    {
       myList.remove(number);
+    }
   }
+  
 }
+
+
 
 void RunCursorList(char* filename)
 {
+  ifstream inf;
+  inf.open(filename);
+  string str;
+  char command;
+  int number;
+  
+  while(inf >> str)
+  {
+    parseCommand(str, command, number);
+    if (command == 'i')
+    {
+    }
+    else
+    {
+    }
+  }
   
   
 }
 
 void RunStackAr(char* filename)
 {
+  ifstream inf;
+  inf.open(filename);
+  string str;
+  char command;
+  int number;
+  
+  while(inf >> str)
+  {
+    parseCommand(str, command, number);
+    if (command == 'i')
+    {
+    }
+    else
+    {
+    }
+  }
   
 }
 
 void RunStackLi(char* filename)
 {
+  ifstream inf;
+  inf.open(filename);
+  string str;
+  char command;
+  int number;
+  
+  while(inf >> str)
+  {
+    parseCommand(str, command, number);
+    if (command == 'i')
+    {
+    }
+    else
+    {
+    }
+  }
   
 }
 
 void RunQueueAr(char* filename)
 {
+  ifstream inf;
+  inf.open(filename);
+  string str;
+  char command;
+  int number;
+  
+  while(inf >> str)
+  {
+    parseCommand(str, command, number);
+    if (command == 'i')
+    {
+    }
+    else
+    {
+    }
+  }
   
 }
 
 void RunSkipList(char* filename)
 {
+  ifstream inf;
+  inf.open(filename);
+  string str;
+  char command;
+  int number;
+  
+  while(inf >> str)
+  {
+    parseCommand(str, command, number);
+    if (command == 'i')
+    {
+    }
+    else
+    {
+    }
+  }
   
 }
 
@@ -89,7 +190,7 @@ int main(int argc, char **argv)
   cout << "File name >> ";
   cin >> filename;
   cout << "\n";
-  
+
   do
   {
     choice = getChoice();
